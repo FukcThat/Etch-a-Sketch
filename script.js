b2688872db8a331071ed113abb8d8cdfc75f0a4c
FukcThat <63652e636f6465@gmail.com>
2024-02-10 00:10:50 +0100

    Made a grid and its lovely

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(5);
+const GridContainer = document.querySelector(".GridContainer")
+
+for (var row = 0; row < 16 ; row++) {
+    
+    let Row = document.createElement("div")
+    Row.classList.add("Row")
+
+    for (var col = 0; col < 16; col++)
+    {
+        let Box = document.createElement("div")
+        Box.classList.add("Box")
+
+        Row.appendChild(Box)
+    }
+    GridContainer.appendChild(Row)
+    
+}
