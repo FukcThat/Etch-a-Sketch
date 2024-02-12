4a5b6aeb6a209bee3e443682a2edd59e768c5065
FukcThat <63652e636f6465@gmail.com>
2024-02-13 00:19:20 +0100

    Added drawing by holding left mousekey. Added Clear Canvas button.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const GridContainer = document.querySelector(".GridContainer")
+const resetButton = document.getElementById('resetButton');
+let isDrawing = false;
+
+function resetGrid() {
+    const boxes = document.querySelectorAll('.Box');
+    boxes.forEach(box => {
+        box.style.backgroundColor = 'pink'; 
+    });
+}
+
+resetButton.addEventListener('click', resetGrid);
 
 for (var row = 0; row < 16 ; row++) {
     
@@ -10,8 +21,25 @@ for (var row = 0; row < 16 ; row++) {
         let Box = document.createElement("div")
         Box.classList.add("Box")
 
+        Box.addEventListener('click', () => {
+            Box.style.backgroundColor = '#A53060';
+        });
+
+        Box.addEventListener('mouseover', () => {
+            if (isDrawing) {
+                Box.style.backgroundColor = '#A53060';
+            }
+        });
+
         Row.appendChild(Box)
     }
     GridContainer.appendChild(Row)
     
 }
+
+document.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    isDrawing = true;
+});
+
+document.addEventListener('mouseup', () => isDrawing = false);
